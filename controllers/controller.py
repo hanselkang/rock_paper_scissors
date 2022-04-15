86ede93b3d2421f668b53c9f8c4c5af4267816a2
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-04-15 07:45:20 +0100

    match success

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,6 +3,7 @@ from app import app
 from models.game_play import game_turn, result_match, games, game_turn
 from models.player import *
 from models.game import Game
+from itertools import islice
 
 
 @app.route('/')
@@ -18,17 +19,24 @@ def game_play_1():
     player1 = request.form['player1']
     player1_choice = request.form['player1_choice']
     game_turn(player1, player1_choice)
-    return render_template('game_play2.html', title="Let's Start", games=games)
-
-
-@app.route('/games2')
-def game_play_2_page():
-    return render_template('game_play2.html', title="Let's Start", games=games)
-
-@app.route('/games2', methods=['POST'])
-def game_play_2():
     print(games)
-    player2 = request.form['player2']
-    player2_choice = request.form['player2_choice']
-    game_turn(player2, player2_choice)
-    return result_match
+    if len(games) <2:
+        return redirect('/games')
+    elif len(games) == 2:
+        first = list(games.values())[0]
+        second = list(games.values())[1]
+        return result_match(first, second)
+    else:
+        return redirect('/')
+    
+
+# @app.route('/games2')
+# def game_play_2_page():
+#     return render_template('game_play2.html', title="Let's Start", games=games)
+
+# @app.route('/games2', methods=['POST'])
+# def game_play_2():
+#     player2 = request.form['player2']
+#     player2_choice = request.form['player2_choice']
+#     game_turn(player2, player2_choice)
+    

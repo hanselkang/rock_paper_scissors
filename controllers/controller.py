94f4062ff4936b631854347c5a4b70c62aa1b14f
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-04-15 15:41:01 +0100

    css start

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,19 +1,18 @@
 from flask import render_template, request, redirect
 from app import app
-from models.game_play import result_route, game_turn, result_match, games, game_turn
+from models.game_play import empty_dict, game_turn, result_match, games, game_turn
 from models.player import *
 from models.game import Game
-from itertools import islice
 
 
 @app.route('/')
 def index():
-     return render_template('index.html', title='Rock Paper Scissors')
+    return render_template('index.html', title='Rock Paper Scissors')
 
 
 @app.route('/games')
 def game_play_1_page():
-     return render_template('game_play.html', title="Let's Start", games=games)
+    return render_template('game_play.html', title="Let's Start", games=games)
 
 
 @app.route('/games', methods=['POST'])
@@ -21,11 +20,12 @@ def game_play():
     player = request.form['player']
     player_choice = request.form['player_choice']
     game_turn(player, player_choice)
-    print(games)
-    if len(games) <2:
+
+    if len(games) < 2:
         return redirect('/games')
     elif len(games) == 2:
         first = list(games.keys())[0]
         second = list(games.keys())[1]
-        return result_match(first, second)
-         
+        result_match(first, second)
+        return render_template('game_result.html', title="Result", games = games, a = result_match(first,second))
+       
--- models/game_play.py
+++ models/game_play.py
@@ -1,13 +1,17 @@
 from models.player import Player
 from models.game import Game
 
+player1_score = 0
+player2_score = 0
 games = {}
 
+
+
 def game_turn(player, choice):
-    games[player]=choice
+    games[player] = choice
 
 
-def result_match(name1,name2):
+def result_match(name1, name2):
 
     if games[name1] == games[name2]:
         return "Draw"
@@ -19,3 +23,8 @@ def result_match(name1,name2):
         return list(games.keys())[0] + " wins by playing " + games[name1]
 
     return list(games.keys())[1] + " wins by playing " + games[name2]
+
+
+
+def empty_dict(dict):
+    return dict.clear()
